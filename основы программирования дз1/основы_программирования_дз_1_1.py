13179c65194b1067acf1e5c2a6610bb098aa5e18
XXXStasnislavXXX <stasperfiliyev@gmail.com>
2022-09-21 15:16:09 +0600

    основы программирования дз1_1 изменение на ввод произвольного числа

--- основы программирования дз1/основы_программирования_дз_1_1.py
+++ основы программирования дз1/основы_программирования_дз_1_1.py
@@ -2,9 +2,10 @@
 Составить блок-схему нахождения среднего арифметического из 4-х чисел
 """
 
-numbers = [1, 2, 3, 4]
+
+numbers = list(range(1, (int(input('введите ваше число ')) + 1)))
 for n in numbers:
     n = (sum(numbers))
     mid_arf = n/len(numbers)
-    print(mid_arf)
+print(mid_arf)
 
