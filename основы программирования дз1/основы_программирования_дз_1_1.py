49df6a721572d1d46d6f078c7743077f45cb2de3
XXXStasnislavXXX <stasperfiliyev@gmail.com>
2022-09-21 13:27:48 +0600

    основы программирования дз1_1

---
+++ основы программирования дз1/основы_программирования_дз_1_1.py
@@ -0,0 +1,10 @@
+"""
+Составить блок-схему нахождения среднего арифметического из 4-х чисел
+"""
+
+numbers = [1, 2, 3, 4]
+for n in numbers:
+    n = (sum(numbers))
+    mid_arf = n/len(numbers)
+    print(mid_arf)
+
